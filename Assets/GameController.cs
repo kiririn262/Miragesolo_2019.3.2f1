7bc1036fea95bb653b9036718401023b331bed5d
kiririn262 <kiririn262+github@gmail.com>
2021-10-07 13:54:02 +0900

    Controller_Input_test

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -14,26 +14,28 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(GvrControllerInput.AppButtonDown)
-        if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.App))
+        #if UNITY_EDITOR
+        if(GvrControllerInput.AppButtonDown)
+        //if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.App))
             {
                 Debug.Log("アプリケーションボタンを押したよ！");
             }
-        //if(GvrControllerInput.HomeButtonDown)
-        if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.System))
+        if(GvrControllerInput.HomeButtonDown)
+        //if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.System))
             {
                 Debug.Log("ホームボタンを押したよ！");
             }
-        //if(GvrControllerInput.ClickButtonDown)
-        if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.TouchPadButton))
+        if(GvrControllerInput.ClickButtonDown)
+        //if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.TouchPadButton))
             {
                 Debug.Log("タッチパッドを押したよ！");
             }
-        //if(GvrControllerInput.TouchDown)
-        if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.TouchPadTouch))
+        if(GvrControllerInput.TouchDown)
+        //if(GvrControllerInputDevice.GetButtonDown(GvrControllerButton.TouchPadTouch))
             {
                 Debug.Log("タッチパッドに触れたよ！");
             }
+        #endif
         //Vector2 GvrControllerInput.TouchPos
         //public Vector2 GvrControllerInputDevice.TouchPos;
     }
